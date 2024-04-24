37006de586b377ddd814a7e6d407b313b210a2ce
kingstar1014 <kingstar1014@outlook.com>
2024-04-24 13:57:51 -0700

    set dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Posting } from './pages/Posting'
 
+function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Voda Sports
-      </h1>
-    </>
+    <div>
+      <Posting />
+    </div>
   )
 }
 
---
+++ src/pages/Posting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Posting = () => {
+  return (
+    <div className='text-white text-[20px] font-bold'>
+        Launching Soon
+    </div>
+  )
+}
